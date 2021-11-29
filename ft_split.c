7a99878bb1fd35963b5e87ee2c659273fbdbebd6
Brahim HAJJI BABA <brhajji-@e3r3p12.42.fr>
2021-11-29 13:48:39 +0100

    warmachine + norme ok

--- ft_split.c
+++ ft_split.c
@@ -6,23 +6,23 @@
 /*   By: brhajji- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/26 11:40:35 by brhajji-          #+#    #+#             */
-/*   Updated: 2021/11/28 13:45:47 by brhajji-         ###   ########.fr       */
+/*   Updated: 2021/11/29 13:46:40 by brhajji-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include<stdlib.h>
 #include"libft.h"
 
-static int	nb_mot(char const *str, char c)
+static int	nb_mot(char const *str, char c, size_t t)
 {
-	int	i;
-	int	nb;
+	size_t	i;
+	int		nb;
 
 	i = 0;
 	nb = 0;
-	while (str[i])
+	while (i < t && str[i])
 	{
-		if (str[i] != c && (str[i - 1] == c || i == 0))
+		if (str[i] != c && (i == 0 || str[i - 1] == c))
 			nb++;
 		i++;
 	}
@@ -38,10 +38,10 @@ static char	*put_in_tab(char const *str, char c)
 	x = 0;
 	i = 0;
 	tab = NULL;
-	while (str[x] == c && str[x])
+	while (str[x] != c && str[x])
 		x++;
 	tab = malloc (sizeof (char) * (x + 1));
-	while (str[i] != c && str[i])
+	while (i < x && str[i] != c && str[i])
 	{
 		tab[i] = str[i];
 		i++;
@@ -52,7 +52,7 @@ static char	*put_in_tab(char const *str, char c)
 
 char	**ft_split(char const *str, char c)
 {
-	int		i;
+	size_t	i;
 	int		taille;
 	int		x;
 	char	**tab;
@@ -61,11 +61,11 @@ char	**ft_split(char const *str, char c)
 		return (NULL);
 	taille = 0;
 	i = -1;
-	x = nb_mot(str, c);
+	x = nb_mot(str, c, ft_strlen(str));
 	tab = malloc (sizeof (char *) * (x + 1));
 	if (!tab)
 		return (NULL);
-	while (str[++i])
+	while (++i < ft_strlen(str) && str[i])
 	{
 		while (str[i] == c)
 			i++;
